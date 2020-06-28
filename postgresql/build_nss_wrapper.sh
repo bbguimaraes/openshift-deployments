d53a6273318d236950ca291120a1c5baaebb4025
Bruno Barcarol Guimarães <bbgstb@gmail.com>
2020-06-28 11:50:26 +0000

    postgresql: silence curl

--- postgresql/build_nss_wrapper.sh
+++ postgresql/build_nss_wrapper.sh
@@ -4,7 +4,8 @@ set -eu
 PKG=nss_wrapper-1.1.11
 BUILD_DIR=/tmp
 BUILD_PKGS='gcc make cmake'
-curl "https://ftp.samba.org/pub/cwrap/$PKG.tar.gz" | tar -C "$BUILD_DIR/" -xz
+curl --silent --show-error "https://ftp.samba.org/pub/cwrap/$PKG.tar.gz" \
+    | tar -C "$BUILD_DIR/" -xz
 mkdir "$BUILD_DIR/$PKG/build"
 cd "$BUILD_DIR/$PKG/build"
 pacman --noconfirm -Syu $BUILD_PKGS
